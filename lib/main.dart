8e304e8aecfffee5148f66689eef8d12771f48ce
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-05-28 11:46:23 -0300

    Add environment vars and parse server config

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:parse_server_sdk/parse_server_sdk.dart';
 
-void main() {
+Future main() async {
+  await dotenv.load(fileName: '.env');
   runApp(MyApp());
+
+  await Parse().initialize(
+    dotenv.env['APP_ID'],
+    dotenv.env['SERVER_URL'],
+    clientKey: dotenv.env['CLIENT_KEY'],
+    autoSendSessionId: true,
+    debug: true
+  );
+
+  final category = ParseObject('Categories')
+    ..set('Title', 'Camisetas')
+    ..set('Position', 2);
+
+  final response = await category.save();
+
+  print(response.success);
 }
 
 class MyApp extends StatelessWidget {
